39d4e79cbd10d11d43439cb4f006907c98eabed5
JRScelza <scelzajr@gmail.com>
2017-02-03 11:23:53 -0500

    max BIC score used to identify ideal K

--- KMeans.py
+++ KMeans.py
@@ -104,25 +104,29 @@ plt.xlabel("# clusters")
 plt.ylabel("# BIC")
 
 
-#k = 3
-#kmeans = cluster.KMeans(n_clusters=k)
-#kmeans.fit(data)
-#
-#labels = kmeans.labels_
-#centroids = kmeans.cluster_centers_
-#
-#
-#for i in range(k):
-#    # select only data observations with cluster label == i
-#    ds = data[np.where(labels==i)]
-#    # plot the data observations
-#    pyplot.plot(ds[:,0],ds[:,1],'o')
-#    # plot the centroids
-#    lines = pyplot.plot(centroids[i,0],centroids[i,1],'kx')
-#    # make the centroid x's bigger
-#    pyplot.setp(lines,ms=15.0)
-#    pyplot.setp(lines,mew=2.0)
-#pyplot.show()
+
+k = (BIC.index(max(BIC))) + 1
+
+
+kmeans = cluster.KMeans(n_clusters=k)
+kmeans.fit(data)
+
+labels = kmeans.labels_
+centroids = kmeans.cluster_centers_
+
+
+for i in range(k):
+    # select only data observations with cluster label == i
+    ds = data[np.where(labels==i)]
+    # plot the data observations
+    plt.figure(2)
+    pyplot.plot(ds[:,0],ds[:,1],'o')
+    # plot the centroids
+    lines = pyplot.plot(centroids[i,0],centroids[i,1],'kx')
+    # make the centroid x's bigger
+    pyplot.setp(lines,ms=15.0)
+    pyplot.setp(lines,mew=2.0)
+pyplot.show()
 
 
 
